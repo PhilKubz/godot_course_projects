9f49ebc2c8917547c4698492556394540f7f56c5
PhilKubsz <philip.kubisz@gmail.com>
2023-09-24 18:55:07 -0700

    3D : Collision : Functional simple demo project

--- 3D/Collision/Scripts/player.gd
+++ 3D/Collision/Scripts/player.gd
@@ -7,3 +7,8 @@ func _physics_process(delta):
 		linear_velocity.x = -move_speed
 	if Input.is_action_pressed("move_right"):
 		linear_velocity.x = move_speed
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Tree"):
+		get_tree().reload_current_scene()
